5cc01f61e61e4a70e0697f1093d13cc43057793e
asif <asif0531@live.com>
2018-01-30 12:01:10 +0600

    initial

---
+++ Calculator.Lib/BasicCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Lib
+{
+    public class BasicCalculator
+    {
+
+        public double Add(double a, double b) {
+            return a + b;
+        }
+
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Div(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
