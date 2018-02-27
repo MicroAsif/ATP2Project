9f62d03adc276a11268cb3fd26a0da743cb81490
Sk. Samiur Rahman <sksami4456@gmail.com>
2018-02-27 22:11:23 +0600

    RegistrationViewModel added

---
+++ ATPProject/DoctorConsult.Core/ViewModel/RegistrationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorConsult.Core.ViewModel
+{
+    public class RegistrationViewModel
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string BirthDay { get; set; }
+        public string BloodGroup { get; set; }
+        public string Gender { get; set; }
+        public string Area { get; set; }
+        public string District { get; set; }
+        public string Division { get; set; }
+    }
+}
