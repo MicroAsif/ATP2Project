7371c5d3fe419913d0113b37c389079fd15d3d7d
Sk. Samiur Rahman <sksami4456@gmail.com>
2018-02-28 17:28:56 +0600

    RegistrationView added[cshtml page added for registration users]

--- ATPProject/DoctorConsult.Core/ViewModel/RegistrationViewModel.cs
+++ ATPProject/DoctorConsult.Core/ViewModel/RegistrationViewModel.cs
@@ -13,11 +13,6 @@ namespace DoctorConsult.Core.ViewModel
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string BirthDay { get; set; }
-        public string BloodGroup { get; set; }
-        public string Gender { get; set; }
-        public string Area { get; set; }
-        public string District { get; set; }
-        public string Division { get; set; }
+        
     }
 }
