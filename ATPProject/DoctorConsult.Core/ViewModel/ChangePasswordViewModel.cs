d614447692291e06465693f9dd8344c80103880f
Asif Rahman <asif0531@live.com>
2018-03-08 02:00:15 +0600

    doctor dashboard done

---
+++ ATPProject/DoctorConsult.Core/ViewModel/ChangePasswordViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorConsult.Core.ViewModel
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(4)]
+        public string NewPassword { get; set; }
+        [Compare("NewPassword")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
