c0318e7842739d40c37353d1a30739bfc2717e33
Asif Rahman <asif0531@live.com>
2018-03-08 05:00:50 +0600

    prescribtion form created

---
+++ ATPProject/DoctorConsult.Core/Model/MedicalTestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorConsult.Core.Model
+{
+    public class MedicalTestModel
+    {
+        [Required]
+        public string TestName { get; set; }
+    }
+}
---
+++ ATPProject/DoctorConsult.Core/Model/MedicineModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorConsult.Core.Model
+{
+    public class MedicineModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Manufacture { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public int Days{ get; set; }
+        [Required]
+        public string DailyTimes { get; set; }
+    }
+}
