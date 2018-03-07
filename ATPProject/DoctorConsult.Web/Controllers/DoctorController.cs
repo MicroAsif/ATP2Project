608aedf950a45634931fec08e9cc3c69c724f02d
Asif Rahman <asif0531@live.com>
2018-03-08 02:37:04 +0600

    doctor view model added

--- ATPProject/DoctorConsult.Web/Controllers/DoctorController.cs
+++ ATPProject/DoctorConsult.Web/Controllers/DoctorController.cs
@@ -10,17 +10,10 @@ namespace DoctorConsult.Web.Controllers
     public class DoctorController : Controller
     {
         [HttpGet]
-        public ActionResult Index(DoctorViewModel doctor)
-        {
-            return View(model: new DoctorViewModel() { Id = 1, FirstName = "John", LastName = "Abraham", Email = "john@aiub.edu", Password = "1234", Bithdate = Convert.ToDateTime("01-01-1994"), BloodGroup = "A+", Gender = "Male", Specialist = "Pain management", Location = "Kuril, Kuratoli", ContactNumber = "015XXXXXXXX", Image = "no image" });
-        }
-
-        public ActionResult Dashboard()
+        public ActionResult Index()
         {
             return View();
         }
-
-
         public ActionResult ChangePassword()
         {
             return View();
