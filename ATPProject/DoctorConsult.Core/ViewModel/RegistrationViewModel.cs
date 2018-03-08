0c006c4cef7343cc32020a2e4236f20e254cc8ee
Sk. Samiur Rahman <sksami4456@gmail.com>
2018-03-09 00:10:04 +0600

    Registration problem solved

--- ATPProject/DoctorConsult.Core/ViewModel/RegistrationViewModel.cs
+++ ATPProject/DoctorConsult.Core/ViewModel/RegistrationViewModel.cs
@@ -9,17 +9,20 @@ namespace DoctorConsult.Core.ViewModel
 {
     public class RegistrationViewModel
     {
-        
-        public string Name { get; set; }
-        
         [Required]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email can't be empty")]
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression(pattern: "(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{4,8})$")]
+        [RegularExpression(pattern: "(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,50})$", ErrorMessage = "At least 6 characters with at least 1 number")]
+        //[StringLength(255, MinimumLength = 6)]
         public string Password { get; set; }
+        [Required]
         [Compare("Password")]
         public string ConfirmPassword { get; set; }
+        [Required]
         public string Type { get; set; }
 
     }
