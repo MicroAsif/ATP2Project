13229ca78e9f2dcf4dc499b2efa8f412875bf10c
Jame <jameahmed125@yahoo.com>
2018-03-11 02:24:44 +0600

    Admin Controller, Layout, Navigation, Doctor List, Patient List, Create Campaign, Campaign List & CreateCampaignViewModel Added

---
+++ ATPProject/DoctorConsult.Core/ViewModel/CreateCampViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorConsult.Core.ViewModel
+{
+    public class CreateCampViewModel
+    {
+        [Required(ErrorMessage = "Campaign location cannot be empty")]
+        public string CampaignLocation;
+
+        [Required(ErrorMessage = "Campaign Duration cannot be empty")]
+        public string CampaignDuration;
+
+        [Required(ErrorMessage = "Campaign Date cannot be empty")]
+        public DateTime CampaignDate;
+
+        public double CampaignRent;
+    }
+}
---
+++ ATPProject/DoctorConsult.Web/Controllers/AdminController.cs
@@ -0,0 +1,63 @@
+﻿using DoctorConsult.Core.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoctorConsult.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DoctorList(List<DoctorProfileViewModel> doctors)
+        {
+            // Demo value
+            doctors = new List<DoctorProfileViewModel>()
+            {
+                new DoctorProfileViewModel(){Id=1, FullName="John", Email="john@doctorconsult.com", Address="North town", Age=24, BloogGroup="B+", Gender="Male", Image="http://www.facebook.com", Location="Kuril Kuratoli", NewFee=800, OldFee=500, Phone="01521434331", Specialist="Pain Management" },
+
+                new DoctorProfileViewModel(){Id=1, FullName="John", Email="john@doctorconsult.com", Address="North town", Age=24, BloogGroup="B+", Gender="Male", Image="http://www.facebook.com", Location="Kuril Kuratoli", NewFee=800, OldFee=500, Phone="01521434331", Specialist="Pain Management" }
+            };
+
+            return View(model: doctors);
+        }
+
+        [HttpGet]
+        public ActionResult PatientList()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CreateCamp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCamp(List<CreateCampViewModel> createCampViewModel)
+        {
+            return View("Index", model: createCampViewModel);
+        }
+
+        [HttpGet]
+        public ActionResult CampList(List<CreateCampViewModel> createCampViewModel)
+        {
+            createCampViewModel = new List<CreateCampViewModel>()
+            {
+                new CreateCampViewModel(){CampaignDuration="3 days", CampaignDate=Convert.ToDateTime("21-04-2018"), CampaignLocation="Banani model town", CampaignRent=50000 },
+
+                new CreateCampViewModel(){CampaignDuration="4 days", CampaignDate=Convert.ToDateTime("21-05-2018"), CampaignLocation="Uttara", CampaignRent=900000 }
+            };
+
+            return View(model: createCampViewModel);
+        }
+    }
+}
