49307b8937f0735e93e8d711ae79cc78824b9ad2
Sk. Samiur Rahman <sksami4456@gmail.com>
2018-03-27 20:15:02 +0600

    Slight design change on create camp view from admin controller

--- ATPProject/DoctorConsult.Web/Controllers/AdminController.cs
+++ ATPProject/DoctorConsult.Web/Controllers/AdminController.cs
@@ -60,9 +60,9 @@ namespace DoctorConsult.Web.Controllers
         {
             var createCampViewModel = new List<CreateCampViewModel>()
             {
-                new CreateCampViewModel(){CampaignDuration="3 days", CampaignDate=Convert.ToDateTime("21-04-2018"), CampaignLocation="Banani model town", CampaignRent=50000 },
+                new CreateCampViewModel(){CampaignDuration="3 days", CampaignDate=Convert.ToDateTime("01-01-2018"), CampaignLocation="Banani model town", CampaignRent=50000 },
 
-                new CreateCampViewModel(){CampaignDuration="4 days", CampaignDate=Convert.ToDateTime("21-05-2018"), CampaignLocation="Uttara", CampaignRent=900000 }
+                new CreateCampViewModel(){CampaignDuration="4 days", CampaignDate=Convert.ToDateTime("01-01-2018"), CampaignLocation="Uttara", CampaignRent=900000 }
             };
 
             return View(model: createCampViewModel);
