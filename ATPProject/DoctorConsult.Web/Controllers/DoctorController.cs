86fc37e993fc9a8921362b12bf519ba0f7402267
Sk. Samiur Rahman <sksami4456@gmail.com>
2018-04-12 01:17:39 +0600

    patient's list modified of doctor's controller

--- ATPProject/DoctorConsult.Web/Controllers/DoctorController.cs
+++ ATPProject/DoctorConsult.Web/Controllers/DoctorController.cs
@@ -24,9 +24,14 @@ namespace DoctorConsult.Web.Controllers
             return View(new DoctorProfileViewModel());
         }
 
-        public ActionResult PatientList()
+        public ActionResult PatientList(List<PatientProfileViewModel> patients)
         {
-            return View();
+            patients = new List<PatientProfileViewModel> {
+            new PatientProfileViewModel() { Name="Samiur Rahman", age="21", BloodGroup="b+", Gender="Male" },
+            new PatientProfileViewModel() { Name = "Asif Rahman", age = "21", BloodGroup = "b+", Gender = "Male" },
+            new PatientProfileViewModel() { Name = "Jamy Ahmed", age = "21", BloodGroup = "b+", Gender = "Male" }
+            };
+            return View(model: patients);
         }
         public ActionResult ConsultRequestList()
         {
